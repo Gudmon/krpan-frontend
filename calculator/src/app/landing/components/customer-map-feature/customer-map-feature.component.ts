f6c9a2e1e4ce58411c6e7e75a6170394c3577d29
Gudmon <gudmonmarcell@gmail.com>
2024-05-17 16:12:36 +0200

    refactor: landing page

--- calculator/src/app/landing/components/customer-map-feature/customer-map-feature.component.ts
+++ calculator/src/app/landing/components/customer-map-feature/customer-map-feature.component.ts
@@ -11,7 +11,7 @@ import { Router } from '@angular/router';
 export class CustomerMapFeatureComponent {
   constructor(readonly router: Router){}
 
-  navigateToPalmsCustomerMap(){
+  navigateToKrpanCustomerMap(){
     this.router.navigate(['customer-map']);
   }
 }
